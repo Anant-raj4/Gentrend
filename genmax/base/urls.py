5139054c519a476c986507f0dd44fbd0aaf5d751
Anant Raj <heyanantraj@gmail.com>
2022-12-16 23:26:28 -0600

    2nd deployment changes

--- genmax/base/urls.py
+++ genmax/base/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.urls import path, include
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf.urls.static import static
+
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
