81af9fb7e362ad0fbcc9734d5b4af0676362a9fd
Anant Raj <heyanantraj@gmail.com>
2022-12-24 18:43:21 -0600

    url fix 1

--- genmax/base/urls.py
+++ genmax/base/urls.py
@@ -17,10 +17,9 @@ from django.urls import path, include
 from . import views
 from django.views.static import serve
 from django.conf.urls.static import static
-
+from django.conf.urls import include, url
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    
+    url(r'^$', views.home, name='home')
 ]
